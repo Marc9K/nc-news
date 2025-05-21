48b8016944d490178d2e647feebec8f2c4ba55b7
M <malekuk9@gmail.com>
2025-05-21 11:05:26 +0100

    fix shows like error message

--- app/components/LikeButton.tsx
+++ app/components/LikeButton.tsx
@@ -1,25 +1,41 @@
-import { Button } from "antd";
+import { Button, message } from "antd";
 import axios from "axios";
 import API from "../../env";
 
 export function LikeButton({ like, setLike, value, icon, text, article_id }) {
+  const [messageApi, contextHolder] = message.useMessage();
+  const error = () => {
+    messageApi.open({
+      type: "error",
+      content: "Your vote didn't count 😢",
+    });
+  };
   return (
-    <Button
-      onClick={() => {
-        let setTo = 0;
-        if (like !== value) {
-          setTo = value;
-        }
-        const incrementBy = like + setTo === 0 ? 2 * setTo : -1 * like + setTo;
-        setLike(setTo);
-        axios.patch(API + "articles/" + article_id, {
-          inc_votes: incrementBy,
-        });
-      }}
-      type={like === value ? "primary" : "default"}
-      icon={icon}
-    >
-      {text}
-    </Button>
+    <>
+      {contextHolder}
+      <Button
+        onClick={() => {
+          let setTo = 0;
+          if (like !== value) {
+            setTo = value;
+          }
+          const incrementBy =
+            like + setTo === 0 ? 2 * setTo : -1 * like + setTo;
+          setLike(setTo);
+          axios
+            .patch(API + "articles/" + article_id, {
+              inc_votes: incrementBy,
+            })
+            .catch((err) => {
+              error();
+              setLike(0);
+            });
+        }}
+        type={like === value ? "primary" : "default"}
+        icon={icon}
+      >
+        {text}
+      </Button>
+    </>
   );
 }
